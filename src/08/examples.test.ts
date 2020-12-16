876ffabcb91a072b61a6d91009ff17018cfffdff
Caleb Mennen <cmennen@cisco.com>
2020-12-16 14:19:22 -0500

    a

--- src/08/examples.test.ts
+++ src/08/examples.test.ts
@@ -1,11 +1,10 @@
 import a from "./a";
 
 test("a examples", () => {
-  expect(a("src/08/example1.txt")).toBe("4");
+  expect(a("src/08/example1.txt")).toBe("5");
 });
 
-// import b from "./b";
-// test("b examples", () => {
-//   expect(b("src/08/example1.txt")).toBe("32");
-//   expect(b("src/08/example2.txt")).toBe("126");
-// });
+import b from "./b";
+test("b examples", () => {
+  expect(b("src/08/example1.txt")).toBe("8");
+});
